1fe57a25b2eb6b3e2f4ce3a6a83f645e2c858b83
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-08 15:51:41 +0530

    Create area_circle.cpp

---
+++ area_circle.cpp
@@ -0,0 +1,13 @@
+// Finding the area of a circle
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float a,r;
+    cout<<"Enter the radius";
+    cin>>r;
+    a=(3.14*r*r)/2;
+    cout<<"The area is "<<a;
+    return 0;
+}
