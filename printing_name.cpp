3b2778f151149a6c8fb446167e408e7adebf1529
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-08 12:55:45 +0530

    Create printing_name.cpp

---
+++ printing_name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"May I Know Your Name? ";
+    cin>>name; //  >>  is an extraction operator
+    cout<<"Hello "<<name;
+    return 0;
+}
