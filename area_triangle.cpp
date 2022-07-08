2a29603ceea7b4bba00fb2a5ffcd1119f8ee052b
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-08 13:17:10 +0530

    Create area_triangle.cpp

---
+++ area_triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float b,h,a; //using float to print the decimal numbers
+    cout<<"Enter the values of Breadth and Height";
+    cin>>b>>h;
+    a=(b*h)/2;
+    cout<<"area is"<<a;
+    return 0;
+}
