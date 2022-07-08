fb1917f0c1d37c83a6d07a74e4a062e8ab42ce9e
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-08 12:50:42 +0530

    Create adding_two_numbers.cpp

---
+++ adding_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter Two Numbers ";
+    cin>>a>>b;
+    c=a+b; //c assign a+b
+    cout<<"Addition is "<<c;
+    return 0;
+}
