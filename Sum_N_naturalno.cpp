0fda2db48e2b1e075a8436b98f84b6956bd1993a
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-08 14:53:08 +0530

    Create Sum_N_naturalno.cpp

---
+++ Sum_N_naturalno.cpp
@@ -0,0 +1,14 @@
+// Finding the sum of first N natural numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"Enter the total number of natural no.s";
+    cin>>n;
+    sum=n*(n+1)/2;
+    cout<<"sum of n natural no. is "<<sum;
+
+
+    return 0;
+}
