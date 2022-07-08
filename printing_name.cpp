ef82eed5ba70291901a2835315765d5bea04470d
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-08 13:03:34 +0530

    Update printing_name.cpp

--- printing_name.cpp
+++ printing_name.cpp
@@ -1,10 +1,16 @@
+//this will read the full name
 #include <iostream>
 using namespace std;
 int main()
 {
     string name;
     cout<<"May I Know Your Name? ";
-    cin>>name; //  >>  is an extraction operator
+    getline(cin,name);
     cout<<"Hello "<<name;
     return 0;
 }
+
+//to print the first name only use cin instead of getline
+
+// cin>>name; //  >>  is an extraction operator
+// cout<<"Hello "<<name;
