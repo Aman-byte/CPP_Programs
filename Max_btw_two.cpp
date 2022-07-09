54903c82ad7ebd318b167127e21827bf7c0f042e
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-09 18:22:00 +0530

    Create Max_btw_two.cpp

---
+++ Max_btw_two.cpp
@@ -0,0 +1,19 @@
+//find max of the two no.
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    if(a > b)
+    {
+        cout << a << " is the greater number";
+    }
+    else
+    {
+        cout << b << " is the greater number";
+    }
+    return 0;
+}
