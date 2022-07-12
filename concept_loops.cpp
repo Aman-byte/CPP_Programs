5724e3c28f6b69e3289534583af94007d6a4b34c
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-07-12 21:34:42 +0530

    Create concept_loops.cpp

---
+++ concept_loops.cpp
@@ -0,0 +1,32 @@
+//While loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0;
+    while (i<10)
+    {
+        cout<<"Hello";
+        i++;
+    }
+    return 0;
+} 
+
+/* Do while loop
+ * int i=0;
+ * do
+ * {
+ * cout<<i<<"Hello\n";
+ * i++;
+ * } while(i<10);
+ * return 0;
+ * }
+ *
+ * For Loop
+ * for(int i=0;i<10;i++)]
+ * {
+ * cout<<i<<"Hello\n";
+ * }
+ * return 0;
+ * }
+ */
